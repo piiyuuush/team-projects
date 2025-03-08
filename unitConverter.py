c0c497e92e6aef9c2cb62758bd6d02fc6062070e
piiyuuush <piyush26102004bhandari@gmail.com>
2025-03-09 00:19:57 +0530

    added mass converter

--- unitConverter.py
+++ unitConverter.py
@@ -1,13 +1,37 @@
 # make functions as following, dont use ai chatbot rather ask me in person
 # conversion should be vice versa aswell not just one sided
-#also try to understand the main function written below (if __name__=="__main__":)
+# also try to understand the main function written below (if __name__=="__main__":)
+# make function for length conversion from : between cm, km, m
+# make function for mass conversion from : between g, kg, tons
+def mass_conversion():
+    from_unit = input("Enter 'from' unit (milligram, gram, kilogram, tons) : ")
+    to_unit = input("Enter 'to' unit (milligram, gram, kilogram, tons) : ")
+    value = int(input("Enter the value : "))
+    if from_unit=="kilogram":
+        value *=1000
+    elif from_unit=="milligram":
+        value /=1000
+    elif from_unit=="tons":
+        value *=1000000
+    elif from_unit=="gram":
+        pass
+    else:
+        print("Invalid 'from' unit")
 
-
-#make function for length conversion from : between cm, km, m
-#make function for mass conversion from : between g, kg, tons
-#make function for time conversion from : between second, minute, hour, day, year
-#make function for temperature conversion from : between celcius, farhaniet
-
+    #now the value is in grams
+    if (to_unit.strip()).lower()=="kilogram":
+        value/=1000
+    elif (to_unit.strip()).lower()=="milligram":
+        value*=1000
+    elif (to_unit.strip()).lower()=="tons":
+        value/=1000000
+    elif (to_unit.strip()).lower()=="gram":
+        pass
+    else:
+        print("Invalid 'to' unit")
+    print(f"value is {value} {to_unit}\n")
+# make function for time conversion from : between second, minute, hour, day, year
+# make function for temperature conversion from : between celcius, farhaniet
 if __name__=="__main__":
     while True:
         try:
@@ -18,13 +42,13 @@ if __name__=="__main__":
             elif choice==2:
                 pass
             elif choice==3:
-                pass
+                mass_conversion()
             elif choice==4:
                 pass
             elif choice==5:
+                print("program exited successfully")
                 break
             else:
                 print("Invalid Choice")
         except ValueError:
             print("Invalid Choice")
-            
