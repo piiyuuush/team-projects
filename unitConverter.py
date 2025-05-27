39bbe42d71876f73af710eec4bee605b9ea3c7c8
Nandkishor Dalvi <Nandkishor.Dalvi@ril.com>
2025-05-27 21:44:20 +0530

    py

--- unitConverter.py
+++ unitConverter.py
@@ -3,7 +3,6 @@
 # also try to understand the main function written below (if __name__=="__main__":)
 # def length_conversion()
 # confllict 03
-# change
 def convert_length():    
     from_unit = input("Enter 'from' unit (cm, m, km) : ")
     to_unit = input("Enter 'to' unit (cm, m, km) : ")
